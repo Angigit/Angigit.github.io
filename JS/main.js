dbf075b8010823cad2d6a33092ac3f34f44894f6
Angigit <kangiex@gmail.com>
2021-12-11 14:16:04 +0100

    footer has been changed

--- JS/main.js
+++ JS/main.js
@@ -766,5 +766,18 @@ function checkTest3() {
 
 // Footer
 
-document.querySelector('.date').innerHTML = formatDate(today, 'mm/dd/yy');
+const today = new Date();
+
+function formatDate(date, format) {
+    const map = {
+        mm: date.getMonth() + 1,
+        dd: date.getDate(),
+        yy: date.getFullYear().toString().slice(-4),
+        yyyy: date.getFullYear()
+    }
+
+    return format.replace(/mm|dd|yy|yyy/gi, matched => map[matched])
+}
+
+document.querySelector('.date').innerHTML = formatDate(today, 'yy/mm/dd');
 
